7fcef913c5867de1f34b123442b0e8cb395646a6
James Washington <Jackson@wassub.com>
2017-11-01 18:14:35 +0100

    Whatsnewinjava8:module1:livecoding1:afirstlampbdaExpression

--- src/org/barry/Main.java
+++ src/org/barry/Main.java
@@ -1,12 +1,33 @@
 package org.barry;
 
+import java.io.File;
+import java.io.FileFilter;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
 
+        FileFilter fileFilter = new FileFilter()
+        {
+            @Override
+            public boolean accept(File pathname)
+            {
+                return pathname.getName().endsWith(".java");
+            }
+        };
 
+        FileFilter filter = (File file) -> file.getName().endsWith(".java");
+
+        File dir = new File("c:/tempo");
+
+        File[] files = dir.listFiles(fileFilter);
+
+        for (File file : files)
+        {
+            System.out.println(file);
+        }
 
 
     }
