f2abcd571061e586cb6411b195c0601de2f3bc9b
James Washington <Jackson@wassub.com>
2017-11-12 00:39:04 +0100

    Intermidails operations on streams. No execution

--- src/org/barry/StreamAndCellectors.java
+++ src/org/barry/StreamAndCellectors.java
@@ -1,5 +1,29 @@
 package org.barry;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
+
 public class StreamAndCellectors
 {
+
+    public static void main(String[] args){
+
+
+        Stream<String> stream = Stream.of("one", "two", "three", "four", "five");
+
+        Predicate<String> p1 = Predicate.isEqual("two");
+        Predicate<String> p2 = Predicate.isEqual("three");
+
+        ArrayList<String> list = new ArrayList<>();
+
+        stream
+                .peek(System.out::println)
+                .filter(p1.or(p2))
+                .peek(list::add);
+
+        System.out.println("Done!");
+        System.out.println("Size = " + list.size());
+        //Just intermidiary operations. No execution.
+    }
 }
