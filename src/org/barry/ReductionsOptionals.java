01388109c669d2dff65f6076a3d3b1e332c83a22
James Washington <Jackson@wassub.com>
2017-11-15 23:17:11 +0100

    whatsnewinjava8:module2:livecoding2:ReductionsAndOptionals

---
+++ src/org/barry/ReductionsOptionals.java
@@ -0,0 +1,30 @@
+package org.barry;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class ReductionsOptionals
+{
+    public static void main(String[] args)
+    {
+//        List<Integer> list = Arrays.asList(10,10,10);
+//        List<Integer> list = Arrays.asList(10,10);
+//        List<Integer> list = Arrays.asList();
+//        List<Integer> list = Arrays.asList(10);
+
+        //Problem: I want to know from this whatis max: But 0 is also involved!
+        List<Integer> list = Arrays.asList(-10, -10);
+
+//        list.stream()
+//                .reduce(0, (i1, i2) -> i1 + i2);
+        Optional<Integer> red =
+                list.stream()
+                        //Solution: remove identity value
+//                        .reduce(Integer::max); // this version doesnt compile(change return to optional)
+                        .reduce(Integer::max);
+
+        System.out.println(red);
+    }
+
+}
